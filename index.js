766d42fe819d063002cded3f44cb816aa78cb86d
Al <alpalma95@gmail.com>
2023-11-05 15:34:19 +0100

    Trying lists

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { defineComponent } from "./src/apa";
 import { derive, html } from "./src";
 import { state } from "./src";
 import { reactive } from "./src/reactive";
+import van from "vanjs-core";
 
 defineComponent(
   "custom-1",
@@ -34,8 +35,12 @@ defineComponent(
     // Under the hood, the whole context is passed as argument, hence we can destructure
     // all the properties that we're defining above
     render: ({ count, inc, props, double }) => html`<div>
-      <!-- This is how we access props from template, if we don't want them to be reactive -->
-      <h2>Count started at: ${props.initial_count}</h2>
+      <!-- Because of how Van works under the hood, we must set the whole property as calculated state.
+    style="color: \${()=>...}" would result into an empty attribute-->
+      <h2 style="${() => (count.val % 2 == 0 ? "color: red" : "color: blue")}">
+        <!-- This is how we access props from template, if we don't want them to be reactive -->
+        Count started at: ${props.initial_count}
+      </h2>
       <div class="red">Count is: ${count} and double is: ${double}</div>
       <button onclick="${() => inc(3)}">++</button>
 
@@ -50,17 +55,32 @@ defineComponent("custom-2", {
   // VanX (addon) offers a reactive function that does the same. However, for the scope of
   // ApaJS, I believe it'd be an overkill, since it implies other helpers. I decided
   // to go with my own lightweight version, based on Van state
-  state: reactive({ count: 0 }),
-  items: [
+  state: reactive({ count: 2 }),
+  items: state([
     { id: 1, text: "Item 1" },
     { id: 2, text: "Item 2" },
-  ],
-  render: ({ state, items }) => html`<div>
+  ]),
+  addItem() {
+    this.state.count.val++;
+    const newItem = {
+      id: this.state.count.val,
+      text: `Item num ${this.state.count.val}`,
+    };
+    this.items.val = [...this.items.val, newItem];
+    van.add(
+      this.list_root,
+      html`<li>Item no: ${newItem.id}: ${newItem.text}</li>`
+    );
+    // this.state.count.val.add({
+    //   id: this.state.count.val,
+    //   text: `Item num ${this.state.count.val}`,
+    // });
+    console.log(this.items.val);
+  },
+  list_root: html`<ul key="random"></ul>`,
+  render: ({ state, items, addItem, list_root }) => html`<div>
     <h1>Test works! ${state.count}</h1>
-    <ul>
-      ${items.map((it) => html`<li>Item ${it.id}: ${it.text}</li>`)}
-    </ul>
-
-    <button onclick="${() => state.count.val++}">Inc reactive</button>
+    ${list_root}
+    <button onclick="${addItem}">Inc reactive</button>
   </div>`,
 });
--- src/apa-html.js
+++ src/apa-html.js
@@ -1,6 +1,7 @@
 import htm from "htm/mini";
 import { tags } from "./van";
 function h(type, props, ...children) {
+  console.log(children);
   const t = tags[type];
   if (props) return t(props, ...children);
   return t(...children);
