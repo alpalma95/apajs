bc5d84e3aa3647e2ff53fb852a0929949dbf31c1
Al <alpalma95@gmail.com>
2023-11-05 12:44:51 +0100

    Better organised exports from van

--- dist/apa.es.js
+++ dist/apa.es.js
@@ -1,13 +1,13 @@
-let { isArray: P } = Array, R = (t) => P(t) ? t.map(
+let { isArray: P } = Array, M = (t) => P(t) ? t.map(
   (e) => typeof e == "string" ? document.createTextNode(e) : e
 ) : typeof t == "string" ? document.createTextNode(t) : t;
-class $ extends HTMLElement {
+class R extends HTMLElement {
   constructor() {
     super();
   }
   connectedCallback() {
     this.getProps();
-    let e = this.shadowRoot ? this.shadowRoot : this, s = R(this.ctx.render(this.ctx) ?? this.render());
+    let e = this.shadowRoot ? this.shadowRoot : this, s = M(this.ctx.render(this.ctx) ?? this.render());
     P(s) ? s.forEach((n) => e.appendChild(n)) : e.appendChild(s), this.onInit();
   }
   attributeChangedCallback(e, s, n) {
@@ -33,16 +33,16 @@ class $ extends HTMLElement {
   render() {
   }
 }
-let B = (t) => {
+let $ = (t) => {
   for (let e of Object.keys(t))
     typeof t[e] == "function" && (t[e] = t[e].bind(t));
   return t;
-}, se = (t, e, s = {}) => {
+}, ie = (t, e, s = {}) => {
   window.customElements.define(
     t,
-    class extends $ {
+    class extends R {
       constructor() {
-        super(), this.ctx = B(e), this.ctx.getHost = () => this, s.shadow && this.attachShadow({ mode: s.shadow });
+        super(), this.ctx = $(e), this.ctx.getHost = () => this, s.shadow && this.attachShadow({ mode: s.shadow });
       }
       onInit() {
         this.ctx.onInit && this.ctx.onInit(this);
@@ -56,7 +56,7 @@ let B = (t) => {
     }
   );
 };
-function G(t) {
+function B(t) {
   for (var e, s, n = arguments, o = 1, i = "", a = "", l = [0], d = function(r) {
     o === 1 && (r || (i = i.replace(/^\s*\n\s*|\s*\n\s*$/g, ""))) ? l.push(r ? n[r] : i) : o === 3 && (r || i) ? (l[1] = r ? n[r] : i, o = 2) : o === 2 && i === "..." && r ? l[2] = Object.assign(l[2] || {}, n[r]) : o === 2 && i && !r ? (l[2] = l[2] || {})[i] = !0 : o >= 5 && (o === 5 ? ((l[2] = l[2] || {})[s] = r ? i ? i + n[r] : n[r] : i, o = 6) : (r || i) && (l[2][s] += r ? i + n[r] : i)), i = "";
   }, c = 0; c < t.length; c++) {
@@ -67,7 +67,7 @@ function G(t) {
   }
   return d(), l.length > 2 ? l.slice(1) : l[1];
 }
-let C = Object, _, f = C.getPrototypeOf, x = document, y, h, u, T = { isConnected: 1 }, H = 1e3, b, A = {}, K = f(T), V = f(f), k = (t, e, s, n) => (t ?? (setTimeout(s, n), /* @__PURE__ */ new Set())).add(e), D = (t, e, s) => {
+let C = Object, _, f = C.getPrototypeOf, x = document, y, h, u, T = { isConnected: 1 }, G = 1e3, b, A = {}, H = f(T), V = f(f), k = (t, e, s, n) => (t ?? (setTimeout(s, n), /* @__PURE__ */ new Set())).add(e), D = (t, e, s) => {
   let n = h;
   h = e;
   try {
@@ -84,7 +84,7 @@ let C = Object, _, f = C.getPrototypeOf, x = document, y, h, u, T = { isConnecte
   for (let e of b)
     e._bindings = m(e._bindings), e._listeners = m(e._listeners);
   b = _;
-}, H), v = {
+}, G), v = {
   get val() {
     return h == null || h.add(this), this._val;
   },
@@ -98,7 +98,7 @@ let C = Object, _, f = C.getPrototypeOf, x = document, y, h, u, T = { isConnecte
       let s = [...e._listeners = m(e._listeners)];
       for (let n of s)
         W(n.f, n.s, n._dom), n._dom = _;
-      e._bindings.length ? y = k(y, e, Q) : e._oldVal = t;
+      e._bindings.length ? y = k(y, e, z) : e._oldVal = t;
     }
   }
 }, I = (t) => ({
@@ -107,7 +107,7 @@ let C = Object, _, f = C.getPrototypeOf, x = document, y, h, u, T = { isConnecte
   _oldVal: t,
   _bindings: [],
   _listeners: []
-}), j = (t) => f(t ?? 0) === v, L = (t) => j(t) ? t.val : t, q = (t) => j(t) ? t.oldVal : t, g = (t, e) => {
+}), j = (t) => f(t ?? 0) === v, K = (t) => j(t) ? t.val : t, L = (t) => j(t) ? t.oldVal : t, g = (t, e) => {
   let s = /* @__PURE__ */ new Set(), n = { f: t }, o = u;
   u = [];
   let i = D(t, s, e);
@@ -129,35 +129,34 @@ let C = Object, _, f = C.getPrototypeOf, x = document, y, h, u, T = { isConnecte
     o != _ && t.append(o);
   }
   return t;
-}, z = (t) => (t._isBindingFunc = 1, t), N = (t) => new Proxy((e, ...s) => {
+}, q = (t) => (t._isBindingFunc = 1, t), N = (t) => new Proxy((e, ...s) => {
   var a;
-  let [n, ...o] = f(s[0] ?? 0) === K ? s : [{}, ...s], i = t ? x.createElementNS(t, e) : x.createElement(e);
+  let [n, ...o] = f(s[0] ?? 0) === H ? s : [{}, ...s], i = t ? x.createElementNS(t, e) : x.createElement(e);
   for (let [l, d] of C.entries(n)) {
     let c = (S) => S ? C.getOwnPropertyDescriptor(S, l) ?? c(f(S)) : _, p = e + "," + l, r = A[p] ?? (A[p] = ((a = c(f(i))) == null ? void 0 : a.set) ?? 0), w = r ? r.bind(i) : i.setAttribute.bind(i, l), O = f(d ?? 0);
     O === v ? g(() => (w(d.val), i)) : O === V && (!l.startsWith("on") || d._isBindingFunc) ? g(() => (w(d()), i)) : w(d);
   }
   return F(i, ...o);
-}, { get: (e, s) => e.bind(_, s) }), J = (t, e) => e ? e !== t && t.replaceWith(e) : t.remove(), Q = () => {
+}, { get: (e, s) => e.bind(_, s) }), J = (t, e) => e ? e !== t && t.replaceWith(e) : t.remove(), z = () => {
   let t = [...y].filter((e) => e._val !== e._oldVal);
   y = _;
   for (let e of new Set(t.flatMap((s) => s._bindings = m(s._bindings))))
     J(e._dom, g(e.f, e._dom)), e._dom = _;
   for (let e of t)
     e._oldVal = e._val;
-}, U = (t, e) => J(t, g(e, t));
-const M = { add: F, _: z, tags: N(), tagsNS: N, state: I, val: L, oldVal: q, derive: W, hydrate: U };
-function X(t, e, ...s) {
-  const n = M.tags[t];
+}, Q = (t, e) => J(t, g(e, t));
+const U = { add: F, _: q, tags: N(), tagsNS: N, state: I, val: K, oldVal: L, derive: W, hydrate: Q }, { state: X, derive: oe, tags: Y } = U;
+function Z(t, e, ...s) {
+  const n = Y[t];
   return e ? n(e, ...s) : n(...s);
 }
-const ie = G.bind(X);
-let { fromEntries: Y, entries: Z } = Object, ee = (t) => typeof t != "object" ? te(t) : Y(Z(t).map(([e, s]) => [e, ee(s)]));
-const { state: te, derive: oe } = M;
+const ne = B.bind(Z);
+let { fromEntries: ee, entries: te } = Object, se = (t) => typeof t != "object" ? X(t) : ee(te(t).map(([e, s]) => [e, se(s)]));
 export {
-  $ as ReactiveWC,
-  se as defineComponent,
+  R as ReactiveWC,
+  ie as defineComponent,
   oe as derive,
-  ie as html,
-  ee as reactive,
-  te as state
+  ne as html,
+  se as reactive,
+  X as state
 };
