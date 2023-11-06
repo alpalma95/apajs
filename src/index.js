3ef190963135a13a9704663337db9a415c1c3ba7
Al <alpalma95@gmail.com>
2023-11-06 16:35:45 +0100

    rename html file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { ReactiveWC, defineComponent } from "./apa";
-import { html } from "./apa-html";
+import { html } from "./html";
 import { state, derive, reactive } from "./van";
 
 export { ReactiveWC, html, state, derive, defineComponent, reactive };
