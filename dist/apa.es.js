848921f239b6fc7cabb4c4143c7aed81eb3ff4a6
Al <alpalma95@gmail.com>
2023-11-15 16:41:49 +0100

    Cleanup and build

--- dist/apa.es.js
+++ dist/apa.es.js
@@ -1,54 +1,41 @@
-let { isArray: l } = Array, { fromEntries: C, entries: j, keys: a } = Object, m = (r, t, s) => {
-  r.addEventListener(t.slice(1), s);
-}, E = (r, t) => {
-  a(t).forEach((s) => {
-    if (s.startsWith("@")) {
-      m(r, s, t[s]);
-      return;
-    }
-    r.setAttribute(s, t[s]);
-  });
-}, d = (r) => {
-  if (typeof r != "object")
-    return document.createTextNode(r);
-  const t = document.createElement(r.type);
-  return r.props && E(t, r.props), r.children && r.children.map(
-    (s) => l(s) ? s.map(d) : d(s)
-  ).forEach((s) => {
-    l(s) ? s.forEach((o) => t.appendChild(o)) : t.appendChild(s);
-  }), t;
-}, v = (r, t) => {
+let { isArray: b } = Array, { fromEntries: g, entries: a, keys: w } = Object, l = (o, s) => {
+  b(s) ? s.forEach(
+    (r) => typeof r == "string" ? o.appendChild(document.createTextNode(r)) : o.appendChild(r)
+  ) : o.appendChild(
+    typeof s == "string" ? document.createTextNode(s) : s
+  );
+}, C = (o, s) => {
   window.customElements.define(
-    r.tag,
+    o.tag,
     class extends HTMLElement {
       constructor() {
         super(), this.ctx = {
-          onInit: (s = () => {
-          }) => s(),
-          onDestroy: (s = () => {
-          }) => s(),
+          onInit: (r = () => {
+          }) => r(),
+          onDestroy: (r = () => {
+          }) => r(),
           subscribers: [],
-          getHost: () => this,
-          watch: (s, o) => this.ctx.subscribers.push({
-            attributeName: s,
-            cb: o
+          host: this,
+          watch: (r, i) => this.ctx.subscribers.push({
+            attributeName: r,
+            cb: i
           })
-        }, r.shadow && this.attachShadow({ mode: r.shadow });
+        }, o.shadow && this.attachShadow({ mode: o.shadow });
       }
       static get observedAttributes() {
-        return r.observed;
+        return o.observed;
       }
       connectedCallback() {
-        let s = {};
+        let r = {};
         this.getAttributeNames().forEach((e) => {
-          e.startsWith(":") && (s[e.slice(1)] = this.getAttribute(e));
-        }), this.ctx.props = s;
-        let o = t(this.ctx), c = this.shadowRoot ?? this;
-        l(o) ? o.forEach((e) => c.appendChild(d(e))) : c.appendChild(d(o)), this.ctx.onInit();
+          e.startsWith(":") && (r[e.slice(1)] = this.getAttribute(e));
+        }), this.ctx.props = r;
+        let i = s(this.ctx), t = this.shadowRoot ?? this;
+        l(t, i), this.ctx.onInit();
       }
-      attributeChangedCallback(s, o, c) {
-        this.ctx.subscribers.forEach(({ attributeName: e, cb: i }) => {
-          s === e && i(s, o, c);
+      attributeChangedCallback(r, i, t) {
+        this.ctx.subscribers.forEach(({ attributeName: e, cb: c }) => {
+          r === e && c(r, i, t);
         });
       }
       disconnectedCallback() {
@@ -57,44 +44,59 @@ let { isArray: l } = Array, { fromEntries: C, entries: j, keys: a } = Object, m 
     }
   );
 };
-function y(r) {
-  for (var t, s, o = arguments, c = 1, e = "", i = "", n = [0], u = function(h) {
-    c === 1 && (h || (e = e.replace(/^\s*\n\s*|\s*\n\s*$/g, ""))) ? n.push(h ? o[h] : e) : c === 3 && (h || e) ? (n[1] = h ? o[h] : e, c = 2) : c === 2 && e === "..." && h ? n[2] = Object.assign(n[2] || {}, o[h]) : c === 2 && e && !h ? (n[2] = n[2] || {})[e] = !0 : c >= 5 && (c === 5 ? ((n[2] = n[2] || {})[s] = h ? e ? e + o[h] : o[h] : e, c = 6) : (h || e) && (n[2][s] += h ? e + o[h] : e)), e = "";
-  }, f = 0; f < r.length; f++) {
-    f && (c === 1 && u(), u(f));
-    for (var b = 0; b < r[f].length; b++)
-      t = r[f][b], c === 1 ? t === "<" ? (u(), n = [n, "", null], c = 3) : e += t : c === 4 ? e === "--" && t === ">" ? (c = 1, e = "") : e = t + e[0] : i ? t === i ? i = "" : e += t : t === '"' || t === "'" ? i = t : t === ">" ? (u(), c = 1) : c && (t === "=" ? (c = 5, s = e, e = "") : t === "/" && (c < 5 || r[f][b + 1] === ">") ? (u(), c === 3 && (n = n[0]), c = n, (n = n[0]).push(this.apply(null, c.slice(1))), c = 0) : t === " " || t === "	" || t === `
-` || t === "\r" ? (u(), c = 2) : e += t), c === 3 && e === "!--" && (c = 4, n = n[0]);
+function m(o) {
+  for (var s, r, i = arguments, t = 1, e = "", c = "", n = [0], f = function(d) {
+    t === 1 && (d || (e = e.replace(/^\s*\n\s*|\s*\n\s*$/g, ""))) ? n.push(d ? i[d] : e) : t === 3 && (d || e) ? (n[1] = d ? i[d] : e, t = 2) : t === 2 && e === "..." && d ? n[2] = Object.assign(n[2] || {}, i[d]) : t === 2 && e && !d ? (n[2] = n[2] || {})[e] = !0 : t >= 5 && (t === 5 ? ((n[2] = n[2] || {})[r] = d ? e ? e + i[d] : i[d] : e, t = 6) : (d || e) && (n[2][r] += d ? e + i[d] : e)), e = "";
+  }, u = 0; u < o.length; u++) {
+    u && (t === 1 && f(), f(u));
+    for (var h = 0; h < o[u].length; h++)
+      s = o[u][h], t === 1 ? s === "<" ? (f(), n = [n, "", null], t = 3) : e += s : t === 4 ? e === "--" && s === ">" ? (t = 1, e = "") : e = s + e[0] : c ? s === c ? c = "" : e += s : s === '"' || s === "'" ? c = s : s === ">" ? (f(), t = 1) : t && (s === "=" ? (t = 5, r = e, e = "") : s === "/" && (t < 5 || o[u][h + 1] === ">") ? (f(), t === 3 && (n = n[0]), t = n, (n = n[0]).push(this.apply(null, t.slice(1))), t = 0) : s === " " || s === "	" || s === `
+` || s === "\r" ? (f(), t = 2) : e += s), t === 3 && e === "!--" && (t = 4, n = n[0]);
   }
-  return u(), n.length > 2 ? n.slice(1) : n[1];
+  return f(), n.length > 2 ? n.slice(1) : n[1];
 }
-function g(r, t, ...s) {
-  return { type: r, props: t, children: s };
+function y(o, s, ...r) {
+  const i = document.createElement(o);
+  if (s)
+    for (const [t, e] of a(s))
+      t.startsWith("on-") ? i.addEventListener(t.slice(3), e) : i.setAttribute(t, e);
+  return r && r.forEach((t) => {
+    if (b(t)) {
+      t.forEach((e) => l(i, e));
+      return;
+    }
+    if (typeof t != "object") {
+      const e = document.createTextNode(t);
+      i.appendChild(e);
+      return;
+    }
+    i.appendChild(t);
+  }), i;
 }
-const A = y.bind(g);
-let p = null, x = (r) => (p = r, r()), w = (r) => {
-  let t = /* @__PURE__ */ new Set(), s = () => {
-    p !== null && (t.add(p), p = null);
-  }, o = () => {
-    t.forEach((e) => e());
-  }, c = typeof r == "object" ? Object.fromEntries(
-    Object.entries(r).map(([e, i]) => [
+const j = m.bind(y);
+let p = null, x = (o) => (p = o, o()), E = (o) => {
+  let s = /* @__PURE__ */ new Set(), r = () => {
+    p !== null && (s.add(p), p = null);
+  }, i = () => {
+    s.forEach((e) => e());
+  }, t = typeof o == "object" ? Object.fromEntries(
+    Object.entries(o).map(([e, c]) => [
       e,
-      typeof i == "object" ? w(i) : i
+      typeof c == "object" ? E(c) : c
     ])
-  ) : typeof r == "function" ? { val: x(() => r) } : { val: r };
-  return new Proxy(c, {
-    get(e, i) {
-      return s(), typeof e[i] == "function" ? e[i]() : e[i];
+  ) : typeof o == "function" ? { val: x(() => o) } : { val: o };
+  return new Proxy(t, {
+    get(e, c) {
+      return r(), typeof e[c] == "function" ? e[c]() : e[c];
     },
-    set(e, i, n) {
-      return e[i] = n, o(), !0;
+    set(e, c, n) {
+      return e[c] = n, i(), !0;
     }
   });
 };
 export {
-  v as defineComponent,
+  C as defineComponent,
   x as derive,
-  A as html,
-  w as stream
+  j as html,
+  E as stream
 };
