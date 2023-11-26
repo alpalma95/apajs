00e4dc26fe9a9db1c5442cd55a111ed86c706376
Al <alpalma95@gmail.com>
2023-11-26 18:57:59 +0100

    Split tailwind config file comment into multiple lines

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,3 @@
-// As Twind intellisense plugin doesn't seem to work at the moment, this is a workaround: https://github.com/tw-in-js/vscode-twind-intellisense/issues/24#issuecomment-1670633807
+// As Twind intellisense plugin doesn't seem to work at the moment,
+// this is a workaround:
+// https://github.com/tw-in-js/vscode-twind-intellisense/issues/24#issuecomment-1670633807
