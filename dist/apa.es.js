83507d9e11e7cdbb8293e511e3874921a6f037cb
Al <alpalma95@gmail.com>
2023-11-29 10:44:25 +0100

    Rebuild and change version

--- dist/apa.es.js
+++ dist/apa.es.js
@@ -2,27 +2,27 @@ let m = (e, r) => {
   let [t, s] = e.split("|");
   return [
     t.trim(),
-    s == null ? void 0 : s.split(",").map((n) => {
-      let l = n.trim();
-      if (/^\'|^\"|^\`/.test(l) || l === "$event")
-        return l === "$event" ? l : l.slice(1, -1);
+    s == null ? void 0 : s.split(",").map((l) => {
+      let n = l.trim();
+      if (/^\'|^\"|^\`/.test(n) || n === "$event")
+        return n === "$event" ? n : n.slice(1, -1);
       try {
-        return JSON.parse(l);
+        return JSON.parse(n);
       } catch {
-        return r[l];
+        return r[n];
       }
     })
   ];
-}, w = (e, r, t) => {
-  let [s, n] = m(r.getAttribute(e));
+}, p = (e, r, t) => {
+  let [s, l] = m(r.getAttribute(e));
   if (!s in t.handlers)
     return;
-  let l = t.handlers[s], i = e.slice(1);
-  n ? r.addEventListener(
-    i,
-    (a) => n.at(0) === "$event" ? l(a, ...n.slice(1)) : l(...n)
-  ) : r.addEventListener(i, l);
-}, p = (e, r) => {
+  let n = t.handlers[s], a = e.slice(1);
+  l ? r.addEventListener(
+    a,
+    (i) => l.at(0) === "$event" ? n(i, ...l.slice(1)) : n(...l)
+  ) : r.addEventListener(a, n);
+}, w = (e, r) => {
   let t = e.getAttribute("ref");
   if (t)
     if (t in r.$refs)
@@ -36,9 +36,9 @@ let m = (e, r) => {
 }, E = (e, r) => {
   let t = e.getAttribute("ref");
   r.$refs[t] instanceof Set ? r.$refs[t].delete(e) : delete r.$refs[t];
-}, o = (e, r) => {
+}, h = (e, r) => {
   e.getAttributeNames().filter((t) => t.startsWith(":") || t === "ref").forEach((t) => {
-    t == "ref" && p(e, r), t.startsWith(":") && w(t, e, r);
+    t == "ref" && w(e, r), t.startsWith(":") && p(t, e, r);
   });
 }, g = (e) => {
   let r = e.parentElement;
@@ -48,15 +48,15 @@ let m = (e, r) => {
 }, N = (e) => {
   let r = (t) => {
     t.filter(
-      (n) => g(n.target) == e
-    ).forEach((n) => {
-      if (n.type !== "childList")
+      (l) => g(l.target) == e
+    ).forEach((l) => {
+      if (l.type !== "childList")
         return;
-      let { addedNodes: l, removedNodes: i } = n;
-      i.length && i.forEach((a) => {
-        !a.tagName || !a.getAttribute("ref") || E(a, e.ctx);
-      }), l.length && l.forEach(async (a) => {
-        a.tagName && (o(a, e.ctx), a.childNodes.length > 0 && await u(a, e.ctx));
+      let { addedNodes: n, removedNodes: a } = l;
+      a.length && a.forEach((i) => {
+        !i.tagName || !i.getAttribute("ref") || E(i, e.ctx);
+      }), n.length && n.forEach(async (i) => {
+        i.tagName && (h(i, e.ctx), i.childNodes.length > 0 && await u(i, e.ctx));
       });
     });
   };
@@ -68,17 +68,17 @@ let m = (e, r) => {
     return;
   const t = document.createTreeWalker(e, NodeFilter.SHOW_ELEMENT, {
     /** @param {HTMLElement} node */
-    acceptNode(n) {
-      return n.getAttributeNames().some((l) => l.startsWith(":") || l === "ref") ? NodeFilter.FILTER_ACCEPT : n.tagName.includes("-") ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_SKIP;
+    acceptNode(l) {
+      return l.getAttributeNames().some((n) => n.startsWith(":") || n === "ref") ? NodeFilter.FILTER_ACCEPT : l.tagName.includes("-") ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_SKIP;
     }
   });
   let s = t.currentNode;
   for (; s = t.nextNode(); )
-    o(s, r);
+    h(s, r);
   e.shadowRoot !== null && d(e.shadowRoot, r);
 }, u = (e, r) => new Promise((t) => {
   d(e, r), t();
-}), $ = (e, r, t = { className: HTMLElement, tag: "" }) => {
+}), C = (e, r, t = { className: HTMLElement, tag: "" }) => {
   window.customElements.get(e.tag) || window.customElements.define(
     e.tag,
     class extends t.className {
@@ -91,8 +91,9 @@ let m = (e, r) => {
           host: this,
           $refs: {},
           handlers: {},
-          watch: (s, n, l) => {
-          }
+          watch: (s, l, n) => {
+          },
+          props: {}
         }, e.shadow && (this.shadow = this.attachShadow({ mode: e.shadow })), typeof e.onConstruct == "function" && e.onConstruct(this);
       }
       static get observedAttributes() {
@@ -100,28 +101,30 @@ let m = (e, r) => {
       }
       connectedCallback() {
         let s = {};
-        this.getAttributeNames().forEach((l) => {
-          let i = this.getAttribute(l);
+        this.getAttributeNames().forEach((a) => {
+          let i = this.getAttribute(a);
           try {
-            s[l] = JSON.parse(i);
+            s[a] = JSON.parse(i);
           } catch {
-            s[l] = i;
+            s[a] = i;
           }
         }), this.ctx.props = s, this.ctx.handlers = {};
-        let n = this.shadowRoot ?? this;
-        if (n.innerHTML = r(this.ctx), !e.isStateless) {
+        let l = this.shadowRoot ?? this, n = r(this.ctx);
+        if (n && (l.innerHTML = n), e.isStatic)
+          this.ctx.onInit(l);
+        else {
           (async () => await u(this, this.ctx))();
-          let l = {
+          let a = {
             childList: !0,
             subtree: !0
           };
           N(this).then((i) => {
-            this.shadowRoot && i.observe(this.shadowRoot, l), i.observe(this, l), this.ctx.onInit(n);
+            this.shadowRoot && i.observe(this.shadowRoot, a), i.observe(this, a), this.ctx.onInit(l);
           });
         }
       }
-      attributeChangedCallback(s, n, l) {
-        this.ctx.watch(s, n, l);
+      attributeChangedCallback(s, l, n) {
+        this.ctx.watch(s, l, n);
       }
       disconnectedCallback() {
         this.ctx.onDestroy();
@@ -129,10 +132,10 @@ let m = (e, r) => {
     },
     (() => t.className !== HTMLElement ? { extends: t == null ? void 0 : t.tag } : {})()
   );
-}, { isArray: b } = Array, C = (e, ...r) => {
+}, { isArray: b } = Array, v = (e, ...r) => {
   let t = r.map((s) => s instanceof HTMLElement ? s.outerHTML : b(s) ? s.join(" ") : s);
   return e.reduce(
-    (s, n, l) => s + n + (t[l] ?? ""),
+    (s, l, n) => s + l + (t[n] ?? ""),
     ""
   );
 }, f = null, c = /* @__PURE__ */ new WeakMap();
@@ -161,29 +164,29 @@ let A = (e) => {
   if (Array.isArray(e) || typeof e != "function" && typeof e != "object")
     return { val: e };
   if (typeof e == "function") {
-    let r = h(1);
+    let r = o(1);
     return A(() => r.val = e()), r;
   } else
     return Object.fromEntries(
       Object.entries(e).map(([r, t]) => [
         r,
-        typeof t == "object" || typeof t == "function" ? h(t) : t
+        typeof t == "object" || typeof t == "function" ? o(t) : t
       ])
     );
-}, h = (e) => {
+}, o = (e) => {
   let r = T(e);
   return new Proxy(r, {
-    get(t, s, n) {
-      return R(t, s), Reflect.get(t, s, n);
+    get(t, s, l) {
+      return R(t, s), Reflect.get(t, s, l);
     },
-    set(t, s, n, l) {
-      return t[s] !== n && (Reflect.set(t, s, n, l), L(t, s)), !0;
+    set(t, s, l, n) {
+      return t[s] !== l && (Reflect.set(t, s, l, n), L(t, s)), !0;
     }
   });
 };
 export {
-  $ as define,
+  C as define,
   A as hook,
-  C as html,
-  h as stream
+  v as html,
+  o as stream
 };
--- dist/apa.es.min.d.ts
+++ dist/apa.es.min.d.ts
@@ -1,29 +1,68 @@
 type shadow = "open" | "closed";
-type HTMLElementOrString = HTMLElement | string;
 type props = {
   [key: string]: any;
 };
 
 type watch = (name: String, oldValue: any, newValue: any) => void;
+type Ref = HTMLElement | Set<HTMLElement>;
+interface Refs {
+  [key: string]: Ref;
+}
 
-interface ContextInternal extends Context {}
+interface Handlers {
+  [key: string]: () => any;
+}
 
 export interface Context {
   [key: string]: any;
   onInit?: (host?: HTMLElement) => void;
   onDestroy?: (host?: HTMLElement) => void;
   watch?: watch;
-  getHost(): HTMLElement;
-  props?: props;
+  $refs: Refs | Object;
+  host: HTMLElement;
+  handlers: Handlers | Object;
+  props?: props | Object;
 }
 
+/**
+ * - **Tag**: Selector for our custom component. Nevermind
+ * if we're using an autonomous custom element or not, we shoud
+ * provide one
+ *
+ * - **Shadow:** If set to any value, it'll be created in our custom element
+ *
+ * - **onConstruct**: Host (customElemet) will be passed as argument.
+ * This is especially useful if we need to perform any custom logic
+ * that typically should run inside the constructor.
+ *
+ * - **Observed**: list of observed attributes to be watched during the attributeChangedCallback
+ *
+ * - **isStatic**: Optional parameter to avoid all hydration step
+ */
 export interface Options {
+  tag: string;
   shadow?: shadow;
+  onConstruct?: (host?: HTMLElement) => void;
   observed?: string[];
+  isStatic?: boolean;
 }
 
-export type DefineComponent = (
-  tagName: string,
-  ctx: Context,
-  opt?: Options
+/**
+ * In case we want our custom element to extend an existing one.
+ */
+interface Ext {
+  className?: HTMLElement;
+  tag?: string;
+}
+
+/**
+ * It takes the context as a parameter (where we can set lifecycle methods, as well as handlers for event listeners) and optionally returns a string to be used as innerHTML.
+ *
+ */
+type Component = (ctx: Context) => string | void;
+
+export type Define = (
+  options: Options,
+  component: Component,
+  ext?: Ext
 ) => void;
